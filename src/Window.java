ea18cf98b68d956b91124b7808605c42bf9a06be
Linus Cyanne <dominic.fuerst@t-online.de>
2021-12-12 00:39:47 +0100

    Just an Icon

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 package src;
 
 import java.awt.Canvas;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.File;
 import javax.swing.JFrame;
 import java.awt.Dimension;
 
@@ -14,6 +17,12 @@ public class Window extends Canvas {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+        try {
+            Image icon = ImageIO.read(new File(System.getProperty("user.dir") + "\\rsc\\icon.png"));
+            frame.setIconImage(icon);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         frame.add(engine);
         frame.setVisible(true);
         engine.start();
